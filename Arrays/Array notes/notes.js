7399889da9349b471c5b141935968e3bce64fa54
Suvadeep <suvadeepm0709@gmail.com>
2024-09-29 01:11:15 +0530

    add array notes

--- Arrays/Array notes/notes.js
+++ Arrays/Array notes/notes.js
@@ -1,30 +1,13 @@
-## Array Notes
-
-#### Q:Write a function that reverses the first k elements of the array ? 
-
-**Solution**:
-
-```js
 function reverse(arr, start, end) {
-
   let left = start;
-
   let right = end;
-
   while (left < right) {
-
     let tmp = arr[left];
-
     arr[left] = arr[right];
-
     arr[right] = tmp;
-
     left++;
-
     right--;
-
   }
-
 }
 
 let nums = [1, 2, 3, 4, 5, 6, 7];
@@ -32,5 +15,3 @@ let nums = [1, 2, 3, 4, 5, 6, 7];
 reverse(nums, 0, 2);
 
 console.log(nums);
-```
-
