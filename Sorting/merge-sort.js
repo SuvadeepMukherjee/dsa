b0f6bb3733ae45f7a7b4957b48bcacc65497636c
Suvadeep <suvadeepm0709@gmail.com>
2024-10-01 11:26:52 +0530

    merge sort

---
+++ Sorting/merge-sort.js
@@ -0,0 +1,33 @@
+function merge(arr, low, mid, high) {
+  let temp = [];
+  let left = low;
+  let right = mid + 1;
+  while (left <= mid && right <= high) {
+    if (arr[left] <= arr[right]) {
+      temp.push(arr[left]);
+      left++;
+    } else {
+      temp.push(arr[right]);
+      right++;
+    }
+  }
+  while (left <= mid) {
+    temp.push(arr[left]);
+    left++;
+  }
+  while (right <= high) {
+    temp.push(arr[right]);
+    right++;
+    for (let i = 0; i <= high; i++) {
+      arr[i] = temp[i - low];
+    }
+  }
+}
+
+function mergeSort(arr, low, high) {
+  if (low >= high) return;
+  let mid = Math.floor((low + high) / 2);
+  mergeSort(arr, low, mid);
+  mergeSort(arr, mid + 1, high);
+  merge(arr, low, mid, high);
+}
