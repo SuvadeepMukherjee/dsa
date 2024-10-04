b4612298f7b96e491dea0c5e1a5bd4cf71c21793
Suvadeep <suvadeepm0709@gmail.com>
2024-10-05 01:16:21 +0530

    move zeroes

--- Arrays/Logic Building/move-zeroes-to-end.js
+++ Arrays/Logic Building/move-zeroes-to-end.js
@@ -1,6 +1,14 @@
 var moveZeroes = function (nums) {
-  let left = 0;
-  for (let right = 0; right < nums.length; right++) {
+  //move left pointer to first 0
+  let left = -1;
+  for (let i = 0; i < nums.length; i++) {
+    left++;
+    if (nums[i] === 0) break;
+  }
+  if (left === -1) return;
+
+  //left stays at left most zero , right loops through the entire array
+  for (let right = left + 1; right < nums.length; right++) {
     if (nums[right] !== 0) {
       [nums[left], nums[right]] = [nums[right], nums[left]];
       left++;
