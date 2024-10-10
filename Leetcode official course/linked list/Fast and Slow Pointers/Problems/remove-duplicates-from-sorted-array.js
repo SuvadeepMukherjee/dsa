55d2208e59400f31e1889905a3f282d09e73219a
Suvadeep <suvadeepm0709@gmail.com>
2024-10-10 18:49:24 +0530

    remove duplicates from sorted array

---
+++ Leetcode official course/linked list/Fast and Slow Pointers/Problems/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,11 @@
+var deleteDuplicates = function (head) {
+  let current = head;
+  while (current && current.next) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return head;
+};
